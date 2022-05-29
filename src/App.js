fb1fe46e91c933d70c1ea56b14b7d68b5bafeeaa
Gabrielle Walsh <gabmwalsh@gmail.com>
2022-05-29 14:33:21 -0700

    Added title, click responce of added to favorites, and uniform scroll to top/bottom buttons

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
     const newFavoritesSet = new Set([...Array.from(favoritesArraySet), favoritesIndex]);
     localStorage.setItem("favorites", Array.from(newFavoritesSet));
     setFavoritesArraySet(newFavoritesSet);
+    alert("Added to local Favorites!");
   }
 
   const handleRemoveFavorite =(favoritesIndex) => {
@@ -63,18 +64,19 @@ function App() {
 
   return (
     <div className="App" id="App">
-      <nav>
-        <ul>
-          <li><Link to='/Home'>Home</Link></li>
-          <li><Link to='/Favorites'>Favorites</Link></li>
-          <li><Link to='/About'>About</Link></li>
-          <li><Link to='/Contact'>Contact</Link></li>
-          <li><button onClick={(event) => {
-              document.getElementById('footer').scrollIntoView();
-          }}>Scroll To Bottom</button></li>
-        </ul>
-      </nav>
-
+      <header id="header">
+        <head>
+        <title>Resource Hub</title>
+        </head>
+        <nav>
+          <ul>
+            <li><Link to='/Home'>Home</Link></li>
+            <li><Link to='/Favorites'>Favorites</Link></li>
+            <li><Link to='/About'>About</Link></li>
+            <li><Link to='/Contact'>Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
       <main>
         <Routes>
           <Route path='/Home' element={<Home addFavorite={handleAddFavorite} resourcesListArray={resourcesListArray}/>} />
@@ -86,13 +88,20 @@ function App() {
         </Routes>
       </main>
       <ScrollButton 
-                targetId='App'
-                behavior={'smooth'} 
-                buttonBackgroundColor={'red'}
-                iconType={'arrow-up'}
-                style= {{fontSize: '24px'}}
-            />
-            <footer id="footer"></footer>
+        targetId='App'
+        behavior={'smooth'} 
+        buttonBackgroundColor={'red'}
+        iconType={'arrow-up'}
+        style= {{fontSize: '24px', bottom: '48px'}}
+      />
+      <ScrollButton 
+        targetId='footer'
+        behavior={'smooth'} 
+        buttonBackgroundColor={'red'}
+        iconType={'arrow-up'}
+        style= {{fontSize: '24px', transform: 'rotate(180deg)'}}
+      />
+      <footer id="footer"></footer>
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,13 +11,6 @@ const Contact = (props) => {
                     <li><a href={'mailto:gabmwalsh@gmail.com'} target="_blank">Email</a></li>
                     <li><a href={'https://github.com/SoPreoccupied'} target="_blank">GitHub</a></li>
                     <li><a href={'https://app.slack.com/client/T0351JZQ0/D039PJQ2T6Y'} target="_blank">Slack</a></li>
-                    {/* 
-                    Template of extra styling from my Portfolio
-                    <a style="color: #FFF" role="button"><i class="fab fa-linkedin-in fa-3x animated faa-slow"></i></a>
-                    <a style="color: #FFF" role="button"><i class="fab fa-google fa-3x animated faa-slow"></i></a>
-                    <a style="color: #FFF" role="button"><i class="fab fa-github fa-3x animated faa-slow"></i></a>
-                    <a style="color: #FFF" role="button"><i class="fab fa-slack-hash fa-3x animated faa-slow"></i></a>
-                    */}
                 </ul> 
 
         </div>
