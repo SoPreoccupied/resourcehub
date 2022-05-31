13940c576a37de944bfd9344b03b351bd0d83d1d
Gabrielle Walsh <gabmwalsh@gmail.com>
2022-05-30 17:04:50 -0700

    Added margin to search bar

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ const About = (props) => {
     return (
         <div>
 
-            <h1>About this project.</h1>
+            <h1>About This Project.</h1>
                 <p>Life happens, whether circumstances present as big or small, your educational journey can be altered. Such consideration led to the creation of this Resource Hub.</p>
                 <br />
                 <p>It's purpose is to assist my community in keeping track of data to provide them a personalized learning experience.</p>
